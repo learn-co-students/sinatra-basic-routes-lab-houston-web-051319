170f9e0a3ffc865d6f6fbe81404d15a45849a433
Stefan Scepanovic <scepa91@icloud.com>
2019-06-03 15:49:51 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Stefan"
+    end
+
+    get '/hometown' do 
+        "My hometown is Nis"
+    end
+
+    get '/favorite-song' do 
+        'My favorite song is "Aint no Sunshine"'
+    end
+
+    
 end
