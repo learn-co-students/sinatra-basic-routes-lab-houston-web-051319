dd771c0bfb26af73c92e94867d8907edb71ef955
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-06-03 16:23:29 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        'Hello, World!'
+    end
+
+    get '/name' do
+        "My name is Stefan"
+    end
+
+    get '/hometown' do
+        "My hometown is Nis"
+    end
+
+    get '/favorite-song' do
+        'My favorite song is Old Town Road, duh'
+    end
+
+
+
+
+
 end
